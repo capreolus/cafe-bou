944ebc7ea3aa65d6e8a0a5d5201147c2a045a932
Kaura Peura <kaura.peura@gmail.com>
2023-02-08 22:12:43 +0200

    🔨 Switch from offscreen canvas to canvas
    The site wasn't working on Safari because of missing support for
    OffscreenCanvas

--- src/renderer/image.ts
+++ src/renderer/image.ts
@@ -6,11 +6,11 @@ export function newImageDataFromUrl(url: string): Promise<ImageData> {
             const image = new Image();
             image.addEventListener('error', (e: ErrorEvent) => { reject(new Error(e.message)); });
             image.addEventListener('load', () => {
-                const canvas = new OffscreenCanvas(image.width, image.height);
+                const canvas = document.createElement('canvas');
                 const ctx = canvas.getContext('2d');
 
-                if (!(ctx instanceof OffscreenCanvasRenderingContext2D)) {
-                    reject(new Error('failed to acquire a 2D context to an offscreen canvas'));
+                if (ctx == null) {
+                    reject(new Error('failed to acquire a 2D context to a canvas'));
                     return;
                 }
 
@@ -22,5 +22,5 @@ export function newImageDataFromUrl(url: string): Promise<ImageData> {
         } catch (err) {
             reject(err);
         }
-    });    
+    });
 }
